64d5e4db9b769f064ba9da9fdae9d82fc50be335
Nikesh <nikeshgupta1904@gmail.com>
2025-09-29 12:43:46 +0530

    entrypoint change

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,4 +23,4 @@ echo "Running Alembic migrations..."
 alembic upgrade head
 
 echo "Starting FastAPI app..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
