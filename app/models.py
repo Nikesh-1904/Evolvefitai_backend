54a27d8abd780857a17b5815445245c2ed40994c
Nikesh-1904 <nikeshgupta1904@gmail.com>
2025-10-10 18:38:08 +0000

    fixes

--- app/models.py
+++ app/models.py
@@ -52,7 +52,7 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
 
 
 class OAuthAccount(SQLAlchemyBaseOAuthAccountTableUUID, Base):
-    user_id: uuid.UUID = mapped_column(pgUUID(as_uuid=True), ForeignKey("users.id", ondelete="cascade"), nullable=False)
+    user_id: uuid.UUID = mapped_column(pgUUID(as_uuid=True), ForeignKey("users.id", ondelete="cascade"), nullable=False) # type: ignore
     user: Mapped["User"] = relationship(back_populates="oauth_accounts")
 
 
