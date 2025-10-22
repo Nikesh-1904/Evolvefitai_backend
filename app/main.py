4e22a52e6fe2e61d04b9fbada9dfdea2b727d63b
Nikesh-1904 <nikeshgupta1904@gmail.com>
2025-10-22 08:57:46 +0000

    google fix-1

--- app/main.py
+++ app/main.py
@@ -29,6 +29,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         settings.FRONTEND_URL,
+        "https://evolvefitai-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
